9077019bf6f20294b6b217e520f7bc4ae412561e
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-11 09:55:49 +0500

    update the create_user route to receive request and send response in the desired format using UserResponse model

--- response_model/main.py
+++ response_model/main.py
@@ -1,5 +1,6 @@
 from models.user import UserResponse, UserCreate
 from fastapi import FastAPI
+from fastapi import status
 
 app = FastAPI()
 
@@ -8,10 +9,19 @@ def home():
     return {"message": "hello home"}
 
 
-@app.post("/users")
+@app.post(
+        "/users",
+        response_model=UserResponse,
+        status_code=status.HTTP_201_CREATED
+    )
 def create_user(user: UserCreate):
-    return {
-        "message": "user created successfully",
-        "user": user
+    saved_user = {
+        "id": 1,    # KEY = MODEL FIELD NAME
+        "username": user.username,
+        "email": user.email,
+        "password": user.password,
     }
+    return saved_user
 
+'''response model does not contain password:
+FastAPI automatically removes password from response'''
