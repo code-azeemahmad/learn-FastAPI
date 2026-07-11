b29ac1946e016540d52f544c1b7381825b2b517a
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-11 17:58:06 +0500

    add create_user endpoint using pydantic models to add user in database

--- db_integration/app/routers/users.py
+++ db_integration/app/routers/users.py
@@ -1,31 +1,28 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from app.database.database import get_db
 from sqlalchemy.orm import Session
 from app.models.user import User
+from app.schemas.user import UserCreate, UserResponse
 
 router = APIRouter(prefix="/users", tags=["User"])
 
-@router.post("/", status_code=status.HTTP_201_CREATED)
-def create_user(db: Session = Depends(get_db)):
+@router.post("/", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+def create_user(user_data: UserCreate, db: Session = Depends(get_db)):
+
+    # Convert Pydantic → SQLAlchemy, convert the validated API data into a database object
     user = User(
-        name="Azeem",
-        email="azeem@example.com",
+    name=user_data.name,
+    email=user_data.email,
     )
 
     db.add(user)
     db.commit() 
     db.refresh(user)
 
-    return user
+    return user # FastAPI converts the SQLAlchemy object into: UserResponse, automatically
 
 
-@router.get("/")
-def get_users(
-    db: Session = Depends(get_db),
-):
-    users = db.query(User).all()
 
-    return users
 
 
 '''
