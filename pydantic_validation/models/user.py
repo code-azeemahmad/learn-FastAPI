c14d44c411672253d51f533d63c16c6b61e7a0c0
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-11 08:04:11 +0500

    describe pydantic user model for validation

---
+++ pydantic_validation/models/user.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    name: str
+    email: str
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: str
+
