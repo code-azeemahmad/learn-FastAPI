4929c7dd2a1766225f4585b0d0fca0ddd10155d0
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-11 17:59:44 +0500

    create get_all_users endpoint to fetch all users from db using pydantic UserResponse model

--- db_integration/app/routers/users.py
+++ db_integration/app/routers/users.py
@@ -26,6 +26,18 @@ def get_users(db: Session = Depends(get_db),):
 
     return db.query(User).all()
 
+@router.get("/{user_id}", response_model=UserResponse,)
+def get_user(user_id: int, db: Session = Depends(get_db),):
+
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found",
+        )
+
+    return user
 
 
 
