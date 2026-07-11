c5d3f3206b080d514be797d4f6d2ee0eb38966b0
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-11 09:19:51 +0500

    inherit basemodel, pydantic, Field() feature, numeric constraints, str types, email-validator, nested models

--- pydantic_validation/models/user.py
+++ pydantic_validation/models/user.py
@@ -1,12 +1,30 @@
 from pydantic import BaseModel
+from pydantic import Field
+from pydantic import EmailStr
+
+# class Address(BaseModel):
+#     city: str
+#     country: str
 
 class UserCreate(BaseModel):
-    name: str
-    email: str
+    name: str = Field(
+        min_length=3,
+        max_length=30,
+        description="user name",
+        pattern=r"^[a-zA-Z0-9_]+$"
+    )
+    email: EmailStr
     password: str
+    # address: Address | None = None    # nested models
 
 class UserResponse(BaseModel):
     id: int
     name: str
-    email: str
+    email: EmailStr
 
+
+
+# Field(...) means Field is required
+# Field(default="Ahmad") or stock: int = 0  # default
+# hobby: str | None = None    # optional
+# description="user name"   # Field meta data
