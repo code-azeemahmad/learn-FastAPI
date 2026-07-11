7dced76570311bd289171bebf911042392e555a4
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-11 09:54:35 +0500

    add UserReponse model other than UserCreate model

--- response_model/models/user.py
+++ response_model/models/user.py
@@ -2,12 +2,8 @@ from pydantic import BaseModel
 from pydantic import Field
 from pydantic import EmailStr
 
-# class Address(BaseModel):
-#     city: str
-#     country: str
-
 class UserCreate(BaseModel):
-    name: str = Field(
+    username: str = Field(
         min_length=3,
         max_length=30,
         description="user name",
@@ -15,16 +11,17 @@ class UserCreate(BaseModel):
     )
     email: EmailStr
     password: str
-    # address: Address | None = None    # nested models
 
 class UserResponse(BaseModel):
     id: int
-    name: str
+    username: str
     email: EmailStr
 
+class UserUpdate(BaseModel):
+    pass
 
+class UserLogin(BaseModel):
+    pass
 
-# Field(...) means Field is required
-# Field(default="Ahmad") or stock: int = 0  # default
-# hobby: str | None = None    # optional
-# description="user name"   # Field meta data
+# fastapi autoo validates before sending response
+# Use separate request and response models
