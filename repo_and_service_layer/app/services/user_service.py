126ffbf63237f664774dccd52211de7b5f0e3c28
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-12 10:28:42 +0500

    rename the repository to user_repository in UserService class

--- repo_and_service_layer/app/services/user_service.py
+++ repo_and_service_layer/app/services/user_service.py
@@ -7,27 +7,27 @@ from fastapi import HTTPException, status
 class UserService:
     """Handles business logic related to users."""
 
-    def __init__(self, repository: UserRepository) -> None:
-        self.repository = repository
+    def __init__(self, user_repository: UserRepository) -> None:
+        self.user_repository = user_repository
 
 
     def create_user(self, user_data: UserCreate) -> User:
-        existing_user = self.repository.get_by_email(user_data.email)
+        existing_user = self.user_repository.get_by_email(user_data.email)
 
         if existing_user:
             raise  HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Email already exists"
             )
-        return self.repository.create(user_data)
+        return self.user_repository.create(user_data)
     
 
     def get_users(self) -> list[User]:
-        return self.repository.get_all()
+        return self.user_repository.get_all()
     
 
     def get_user(self, user_id: int) -> User:   # def get_user reuseable in def delete_user and def update_user
-        user = self.repository.get_by_id(user_id)
+        user = self.user_repository.get_by_id(user_id)
 
         if user is None:
             raise HTTPException(
@@ -39,7 +39,7 @@ class UserService:
 
 
     def delete_user(self, user_id: int) -> None:
-        # user = self.repository.get_by_id(user_id)
+        # user = self.user_repository.get_by_id(user_id)
 
         # if user is None:
         #     raise HTTPException(
@@ -49,13 +49,13 @@ class UserService:
 
         user = self.get_user(user_id)
         
-        return self.repository.delete(user)
+        return self.user_repository.delete(user)
     
 
     def update_user(self, user_id: int, user_data: UserUpdate,) -> User:
         user = self.get_user(user_id)
 
-        existing_user = self.repository.get_by_email(user_data.email)
+        existing_user = self.user_repository.get_by_email(user_data.email)
 
         if existing_user and existing_user.id != user.id:
             raise HTTPException(
@@ -63,14 +63,14 @@ class UserService:
                 detail="Email already exists",
             )
 
-        return self.repository.update(user, user_data)
+        return self.user_repository.update(user, user_data)
 
 
     def patch_user(self, user_id: int, user_data: UserPatch,) -> User:
         user = self.get_user(user_id)
 
         if user_data.email is not None:
-            existing_user = self.repository.get_by_email(user_data.email)
+            existing_user = self.user_repository.get_by_email(user_data.email)
 
             if existing_user and existing_user.id != user.id:
                 raise HTTPException(
@@ -78,4 +78,4 @@ class UserService:
                     detail="Email already exists",
                 )
 
-        return self.repository.patch(user, user_data)
+        return self.user_repository.patch(user, user_data)
