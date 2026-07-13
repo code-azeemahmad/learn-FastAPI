29c614e53a805ddd29dbae60f91250af477fb4cb
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-13 11:29:17 +0500

    implement logging middleware

--- middleware/app/main.py
+++ middleware/app/main.py
@@ -5,6 +5,7 @@ from app.database.database import engine
 from app.handlers.exception_handlers import register_exception_handlers
 
 from app.middleware.timing import process_time_middleware
+from app.middleware.logging import logging_middleware
 from app.routers import users, auth
 
 # Import models so SQLAlchemy registers them
@@ -12,6 +13,7 @@ from app.models import user  # noqa: F401
 
 app = FastAPI()
 
+app.middleware("http")(logging_middleware)
 app.middleware("http")(process_time_middleware)
 
 register_exception_handlers(app)
