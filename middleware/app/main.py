cb68ba5195a438bbd67b80101e01cb0270b3496d
code-azeemahmad <azeemahmadd9@gmail.com>
2026-07-13 11:10:43 +0500

    implement timing middleware

--- middleware/app/main.py
+++ middleware/app/main.py
@@ -4,12 +4,15 @@ from app.database.base import Base
 from app.database.database import engine
 from app.handlers.exception_handlers import register_exception_handlers
 
+from app.middleware.timing import process_time_middleware
 from app.routers import users, auth
 
 # Import models so SQLAlchemy registers them
 from app.models import user  # noqa: F401
 
 app = FastAPI()
+
+app.middleware("http")(process_time_middleware)
 
 register_exception_handlers(app)
 app.include_router(users.router)
